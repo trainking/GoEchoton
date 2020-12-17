10c66839ac8c47df595c6dfe51cf2b6ba1efbdcf
trainking <yefulincode@gmail.com>
2020-12-17 21:19:25 +0800

    add 组路由

--- bootstrap/bootstrap.go
+++ bootstrap/bootstrap.go
@@ -17,11 +17,23 @@ type Server struct {
 
 // 启动服务
 func (s *Server) Start() {
+	// 全局中间件
 	for _, f := range s.mFuns {
 		s.e.Use(f)
 	}
+	// 无组路由
 	for _, r := range s.routers {
 		s.e.Add(r.Method, r.Path, r.Handler, r.Middlwares...)
+	}
+	// 路由组
+	for _, g := range router.Groups {
+		_g := s.e.Group(g.Path)
+		for _,_m := range g.Middlwares {
+			_g.Use(_m)
+		}
+		for _,_r := range g.Routers {
+			_g.Add(_r.Method, _r.Path, _r.Handler, _r.Middlwares...)
+		}
 	}
 	s.e.Logger.Fatal(s.e.Start(fmt.Sprintf(":%d", Config.Server.Port)))
 }
