6d2d206bcf587b3ee3f478448cd211971bcb0457
trainking <yefulincode@gmail.com>
2020-12-20 15:37:36 +0800

    add redis

--- go.mod
+++ go.mod
@@ -4,9 +4,10 @@ go 1.15
 
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/go-redis/redis/v8 v8.4.4
 	github.com/labstack/echo/v4 v4.1.17
 	go.mongodb.org/mongo-driver v1.4.4
 	golang.org/x/crypto v0.0.0-20201216223049-8b5274cf687f // indirect
 	golang.org/x/net v0.0.0-20201216054612-986b41b23924 // indirect
-	gopkg.in/yaml.v2 v2.2.8
+	gopkg.in/yaml.v2 v2.3.0
 )
--- handler/index.go
+++ handler/index.go
@@ -3,11 +3,13 @@ package handler
 import (
 	"GoEchoton/config"
 	"GoEchoton/repository"
+	"context"
 	"fmt"
 	"net/http"
 	"time"
 
 	"github.com/dgrijalva/jwt-go"
+	"github.com/go-redis/redis/v8"
 	"github.com/labstack/echo/v4"
 )
 
@@ -25,9 +27,27 @@ type Login_json struct {
 	Password string
 }
 
+var ctx = context.Background()
+
 // 验证账号和密码
 func (l *Login_json) valid() (bool, error) {
-	if l.Username != "jon" || l.Password != "hahha" {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "121.4.65.13:6379",
+		Password: "y123456", // no password set
+		DB:       0,         // use default DB
+	})
+	var username string
+	var password string
+	var err error
+	username, err = rdb.Get(ctx, "username").Result()
+	if err != nil {
+		return false, err
+	}
+	password, err = rdb.Get(ctx, "password").Result()
+	if err != nil {
+		return false, err
+	}
+	if l.Username != username || l.Password != password {
 		return false, nil
 	}
 	return true, nil
