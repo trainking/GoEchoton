b9b87927c6ed9a1ce5dcd0a2c51c6faadf79d7f3
trainking <yefulincode@gmail.com>
2020-12-30 21:06:54 +0800

    add 继承

--- repository/user.go
+++ repository/user.go
@@ -4,15 +4,20 @@ import (
 	"GoEchoton/database"
 )
 
-// UserOP 用户操作接口
-type UserOP interface {
-	Valid(username, password string) (bool, error)
-}
+type (
+	// UserOP 用户操作接口
+	UserOP interface {
+		Repository
 
-// userop op实现
-type userop struct {
-	client *database.Redis
-}
+		// Valid 校验用户名和密码
+		Valid(username, password string) (bool, error)
+	}
+
+	// userop op实现
+	userop struct {
+		client *database.Redis
+	}
+)
 
 // Valid 校验用户名和密码
 func (op *userop) Valid(username, password string) (bool, error) {
@@ -28,6 +33,11 @@ func (op *userop) Valid(username, password string) (bool, error) {
 		return false, nil
 	}
 	return true, nil
+}
+
+// Destory
+func (op *userop) Destory() error {
+	return nil
 }
 
 // NewUserOP 创建OP
