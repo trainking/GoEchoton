7fec6fd3d81115c40fc8c6cfd07dd9d16980f336
trainking <yefulincode@gmail.com>
2021-01-17 00:33:40 +0800

    add common

--- build.py
+++ build.py
@@ -4,26 +4,39 @@ import os
 import subprocess
 import shutil
 
-print("start!")
+def build(project, goos="linux", goarch="amd64"):
+    # save old go env
+    env_goarch = subprocess.check_output("go env get GOARCH")
+    env_goarch = env_goarch[1:-1]
+    env_goos = subprocess.check_output("go env get GOOS")
+    env_goos = env_goos[1:-1]
 
-# check ./bin is exists
-if not os.path.exists("./bin"):
-    os.mkdir("./bin")
+    # change to linux env
+    subprocess.run("go env -w GOARCH=%s GOOS=%s" % (goarch, goos))
+    # building...
+    subprocess.run("go build -o ./bin/%s ./" % project)
+    # copy config file: env.yaml
+    shutil.copyfile('env.yaml', './bin/env.yaml')
 
-# save old go env
-env_goarch = subprocess.check_output("go env get GOARCH")
-env_goarch = env_goarch[1:-1]
-env_goos = subprocess.check_output("go env get GOOS")
-env_goos = env_goos[1:-1]
+    # change back old env
+    subprocess.run("go env -w GOARCH=%s GOOS=%s" % (str(env_goarch, encoding="utf-8"), str(env_goos, encoding="utf-8")))
 
-# change to linux env
-subprocess.run("go env -w GOARCH=amd64 GOOS=linux")
-# building...
-subprocess.run("go build -o ./bin/GoEchoton ./")
-# copy config file: env.yaml
-shutil.copyfile('env.yaml', './bin/env.yaml')
+def main():
+    print("start!")
+    # check ./bin is exists
+    if not os.path.exists("./bin"):
+        os.mkdir("./bin")
 
-# change back old env
-subprocess.run("go env -w GOARCH=%s GOOS=%s" % (str(env_goarch, encoding="utf-8"), str(env_goos, encoding="utf-8")))
+    # find module name
+    pwd = subprocess.check_output("go mod why")
+    pwd = str(pwd, encoding="utf-8")
+    i = pwd.find('\n')
+    project = pwd[i+1:-1]
 
-print("buid!")
+    # to build
+    build(project)
+
+    print("buid!")
+
+if __name__ == "__main__":
+    main()
