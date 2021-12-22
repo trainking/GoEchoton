5587446caa03cecffae38d7646d039972f20e194
trainking <yefulincode@gmail.com>
2021-12-22 23:45:29 +0800

    docs: 增加提交约束规范
    增加模板以及type说明

--- model/param/login.go
+++
@@ -1,7 +0,0 @@
-package param
-
-// LoginUser 登录
-type LoginUser struct {
-	Username string `json:"username"`
-	Password string `json:"password"`
-}
--- pkg/delayq/delayq.go
+++
@@ -1,7 +0,0 @@
-package delayq
-
-type DelayQ struct {
-	DeadletterExchange string // 死信交换机名称
-	DeadletterQueue    string // 死信队列名称
-
-}
