78e72d45a224603d5fe7a05cf216f968211e9293
trainking <yefulincode@gmail.com>
2022-01-01 21:38:36 +0800

    fix: 增加配置加载路径

--- internal/authserver/authserver.go
+++ internal/authserver/authserver.go
@@ -14,7 +14,11 @@ var listenAddr = flag.String("addr", ":5050", "listen address")
 func main() {
 	flag.Parse()
 
-	svcCtx := svc.New(&config.Config{})
+	var conf config.Config
+	if err := apiserver.LoadConfigFile("./configs/authserver.yaml", &conf); err != nil {
+		panic(err)
+	}
+	svcCtx := svc.New(&conf)
 	server := apiserver.New(svcCtx)
 	server.Start(*listenAddr)
 }
