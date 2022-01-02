a55c7f54964e8dca72195d44efa2881a4397abcd
trainking <yefulincode@gmail.com>
2022-01-02 23:31:58 +0800

    feat: 加入多组客户端池判断和修改操作抽象
    - 抽象了一个ClientPool接口
    - arpc客户端加入多组，应对Etcd集群

--- internal/pkg/arpcclient/client.go
+++ internal/pkg/arpcclient/client.go
@@ -1,28 +1,72 @@
 package arpcclient
 
 import (
-	"net"
-	"time"
+	"sync"
 
 	"github.com/lesismal/arpc"
 )
 
+var _clientOnce sync.Once
+var _clientIns *Client
+
 type Client struct {
-	pool *arpc.ClientPool
+	mu     sync.RWMutex
+	pools  map[string]*arpc.ClientPool
+	addrs  []string
+	length int
+
+	rk int
+
+	cp ClientPool
 }
 
 // New 创建连接池
 func New(listenAddr string, size int) (*Client, error) {
-	pool, err := arpc.NewClientPool(func() (net.Conn, error) {
-		return net.DialTimeout("tcp", listenAddr, time.Second*3)
-	}, size)
-	if err != nil {
+	_clientOnce.Do(func() {
+		pools := make(map[string]*arpc.ClientPool)
+		_clientIns = &Client{pools: pools, cp: NewDefaultClientPool()}
+	})
+
+	if err := _clientIns.AddClientPool(listenAddr, size); err != nil {
 		return nil, err
 	}
-	return &Client{pool: pool}, nil
+	return _clientIns, nil
+}
+
+// AddClientPool 增加客户端池
+func (c *Client) AddClientPool(listenAddr string, size int) error {
+	pool, err := c.cp.Create(listenAddr, size)
+	if err != nil {
+		return err
+	}
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.pools[listenAddr] = pool
+	c.addrs = append(c.addrs, listenAddr)
+	c.length++
+	return nil
+}
+
+// DeleteClientPool 删除客户端池
+func (c *Client) DeleteClientPool(listenAddr string) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	delete(c.pools, listenAddr)
+	c.length--
+	keys := make([]string, c.length)
+
+	j := 0
+	for k := range c.pools {
+		keys[j] = k
+		j++
+	}
+	c.addrs = keys
 }
 
 // C 返回客户端实例
 func (c *Client) C() *arpc.Client {
-	return c.pool.Next()
+	c.mu.RLock()
+	pool := c.pools[c.addrs[(c.rk+1)%c.length]]
+	c.mu.RUnlock()
+	return c.cp.Choose(pool)
 }
