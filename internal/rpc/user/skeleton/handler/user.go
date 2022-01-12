ad96d38b41ec11d90f041954e46323390861b3a2
trainking <yefulincode@gmail.com>
2022-01-12 23:09:43 +0800

    feat: 增加RequestID到arpc的处理

--- internal/rpc/user/skeleton/handler/user.go
+++ internal/rpc/user/skeleton/handler/user.go
@@ -5,18 +5,18 @@ import (
 	"GoEchoton/internal/rpc/user/userrpc"
 	"GoEchoton/pkg/arpcx"
 	"context"
-
-	"github.com/lesismal/arpc"
 )
 
 // UserCheckPasswd 检查密码
-func UserCheckPasswd(ctx *arpc.Context) *arpcx.Result {
+func UserCheckPasswd(ctx arpcx.Context) error {
 	var p userrpc.CheckPasswd
 	if err := ctx.Bind(&p); err != nil {
-		return &arpcx.Result{Err: err}
+		return err
 	}
+
 	if err := service.NewUserService().CheckPasswd(context.Background(), &p); err != nil {
-		return &arpcx.Result{Err: err}
+		return err
 	}
-	return &arpcx.Result{}
+
+	return ctx.Write()
 }
---
+++ pkg/arpcx/context.go
@@ -0,0 +1,72 @@
+package arpcx
+
+import (
+	"encoding/json"
+
+	"github.com/lesismal/arpc"
+)
+
+type (
+	Context interface {
+
+		// GetRequestID 获取RequestID
+		GetRequestID() string
+
+		// Bind 将数据绑定
+		Bind(i interface{}) error
+
+		// Write 写入成功返回
+		Write(i ...interface{}) error
+	}
+
+	Request struct {
+		ID   string      `json:"id"`
+		Data interface{} `json:"data"`
+	}
+
+	Result struct {
+		Err  error
+		Data interface{}
+	}
+
+	defaultContext struct {
+		ctx *arpc.Context
+		req *Request
+	}
+)
+
+func NewContext(ctx *arpc.Context) Context {
+	var req Request
+	if err := ctx.Bind(&req); err != nil {
+		panic(err)
+	}
+	return &defaultContext{
+		ctx: ctx,
+		req: &req,
+	}
+}
+
+// GetRequestID 获取RequestID
+func (c *defaultContext) GetRequestID() string {
+	return c.req.ID
+}
+
+// Bind 绑定数据
+func (c *defaultContext) Bind(i interface{}) error {
+	b, err := json.Marshal(c.req.Data)
+	if err != nil {
+		return err
+	}
+	return json.Unmarshal(b, i)
+}
+
+// Write 写出返回结果
+func (c *defaultContext) Write(i ...interface{}) error {
+	if len(i) == 0 {
+		return c.ctx.Write(struct{}{})
+	}
+	if len(i) > 1 {
+		panic("write more than one")
+	}
+	return c.ctx.Write(i[0])
+}
