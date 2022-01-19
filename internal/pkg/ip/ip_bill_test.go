51946dbb266bedffc4bf28c407d2ef50cb7cc4e2
trainking <yefulincode@gmail.com>
2022-01-20 00:05:11 +0800

    test: 增加ip_bill的基准测试

--- internal/pkg/ip/ip_bill_test.go
+++ internal/pkg/ip/ip_bill_test.go
@@ -14,3 +14,10 @@ func TestContains(t *testing.T) {
 	assert.Assert(t, ipBill.Contains("172.0.0.3"))
 	assert.Assert(t, ipBill.Contains("172.0.0.4"))
 }
+
+func BenchmarkContains(b *testing.B) {
+	ipBill := NewIpBill([]string{"127.0.0.1", "172.0.0.1/24"})
+	for i := 0; i < b.N; i++ {
+		ipBill.Contains("127.0.0.1")
+	}
+}
