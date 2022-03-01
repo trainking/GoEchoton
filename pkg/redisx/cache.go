0cb0c848501a387fe67c11214f11520145654e51
xiaoye <yefulincode@gmail.com>
2022-03-01 22:20:41 +0800

    fix: 修复cache的方式

--- pkg/redisx/cache.go
+++ pkg/redisx/cache.go
@@ -14,7 +14,7 @@ import (
 type Cache struct {
 	client *redis.Client
 
-	ttl time.Duration // 缓存过期时间
+	ttl time.Duration // 缓存过期时间, 单位秒
 
 	sg singleflight.Group
 }
@@ -22,12 +22,17 @@ type Cache struct {
 type DoFunc func(ctx context.Context) (interface{}, error)
 
 // NewCache use *redis.Options created.
-func NewCache(options *redis.Options, ttl time.Duration) *Cache {
-	return &Cache{client: redis.NewClient(options), ttl: ttl}
+func NewCache(options *redis.Options, ttl int64) *Cache {
+	return &Cache{client: redis.NewClient(options), ttl: time.Duration(ttl) * time.Second}
 }
 
-// Get cache get. if nil, dofunc() set.
+// Get 获取缓存，使用的redis.String类型缓存，编码解码使用json
 func (c *Cache) Get(ctx context.Context, key string, i interface{}, dofunc DoFunc) error {
+	id := reflect.ValueOf(i)
+	if id.Kind() != reflect.Ptr {
+		return errors.New("i is not a pointer")
+	}
+
 	// fast. 读取缓存
 	v, err := c.client.Get(ctx, key).Result()
 	if err != nil && err != redis.Nil {
@@ -43,13 +48,13 @@ func (c *Cache) Get(ctx context.Context, key string, i interface{}, dofunc DoFun
 		c.client.Del(ctx, key)
 	}
 
-	return c.getSlow(ctx, key, i, dofunc)
+	return c.getSlow(ctx, key, id, dofunc)
 }
 
 // getSlow 无缓存，需要dofunc创建
-func (c *Cache) getSlow(ctx context.Context, key string, i interface{}, dofunc DoFunc) error {
+func (c *Cache) getSlow(ctx context.Context, key string, id reflect.Value, dofunc DoFunc) error {
 	// 请求合并，防止穿透
-	_, err, _ := c.sg.Do(key, func() (interface{}, error) {
+	r, err, _ := c.sg.Do(key, func() (interface{}, error) {
 		ri, err := dofunc(ctx)
 		if err != nil {
 			return nil, err
@@ -61,28 +66,42 @@ func (c *Cache) getSlow(ctx context.Context, key string, i interface{}, dofunc D
 		}
 		c.client.Set(ctx, key, string(b), c.ttl)
 
-		err = json.Unmarshal(b, i)
-		return nil, err
+		return ri, err
 	})
+
+	rd := reflect.ValueOf(r)
+	if rd.Kind() == reflect.Ptr {
+		rd = rd.Elem()
+	}
+	id.Elem().Set(rd)
 
 	return err
 }
 
+// HGetAll 获取缓存，使用redis.Hash类型缓存，仅支持使用基础类型元素的Struct及其指针
 func (c *Cache) HGetAll(ctx context.Context, key string, i interface{}, dofunc DoFunc) error {
+	id := reflect.ValueOf(i)
+	if id.Kind() != reflect.Ptr {
+		return errors.New("i is not a pointer")
+	}
+
 	// fast. 读取缓存
 	mapCmd := c.client.HGetAll(ctx, key)
 	err := mapCmd.Err()
 	if err == nil {
-		if err := mapCmd.Scan(i); err != nil {
-			return err
+		if len(mapCmd.Val()) > 0 {
+			if err := mapCmd.Scan(i); err != nil {
+				return err
+			}
+			return nil
 		}
-		return nil
-	} else if err != redis.Nil {
+
+	} else {
 		return err
 	}
 
 	// slow 加载缓存
-	_, err, _ = c.sg.Do(key, func() (interface{}, error) {
+	r, err, _ := c.sg.Do(key, func() (interface{}, error) {
 		ri, err := dofunc(ctx)
 		if err != nil {
 			return nil, err
@@ -91,36 +110,54 @@ func (c *Cache) HGetAll(ctx context.Context, key string, i interface{}, dofunc D
 		// 反射获取
 		rd := reflect.ValueOf(ri)
 		if rd.Kind() == reflect.Ptr {
-			i = ri
 			rd = rd.Elem()
-		} else {
-			i = &ri
 		}
+
 		td := rd.Type()
 
 		_, err = c.client.Pipelined(ctx, func(rdb redis.Pipeliner) error {
-			for j := 0; j < rd.NumField(); j++ {
-				v := rd.Field(j)
-				t := td.Field(j)
-
-				switch v.Kind() {
-				case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-					rdb.HSet(ctx, key, t.Tag.Get("redis"), v.Int())
-				case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
-					rdb.HSet(ctx, key, t.Tag.Get("redis"), v.Uint())
-				case reflect.Float32, reflect.Float64:
-					rdb.HSet(ctx, key, t.Tag.Get("redis"), v.Float())
-				case reflect.Bool:
-					rdb.HSet(ctx, key, t.Tag.Get("redis"), v.Bool())
-				case reflect.String:
-					rdb.HSet(ctx, key, t.Tag.Get("redis"), v.String())
-				default:
-					return errors.New("no support type")
-				}
+			if err := reflectStructHash(ctx, key, rdb, rd, td); err != nil {
+				return err
 			}
+			rdb.Expire(ctx, key, c.ttl)
 			return nil
 		})
-		return nil, err
+		return ri, err
 	})
+
+	rdv := reflect.ValueOf(r)
+	if rdv.Kind() == reflect.Ptr {
+		rdv = rdv.Elem()
+	}
+	id.Elem().Set(rdv)
 	return err
+}
+
+// reflectStructHash 反射结构字段到redis.Hash
+func reflectStructHash(ctx context.Context, key string, rdb redis.Pipeliner, rd reflect.Value, td reflect.Type) error {
+	for j := 0; j < rd.NumField(); j++ {
+		v := rd.Field(j)
+		t := td.Field(j)
+
+		switch v.Kind() {
+		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+			rdb.HSet(ctx, key, t.Tag.Get("redis"), v.Int())
+		case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+			rdb.HSet(ctx, key, t.Tag.Get("redis"), v.Uint())
+		case reflect.Float32, reflect.Float64:
+			rdb.HSet(ctx, key, t.Tag.Get("redis"), v.Float())
+		case reflect.Bool:
+			rdb.HSet(ctx, key, t.Tag.Get("redis"), v.Bool())
+		case reflect.String:
+			rdb.HSet(ctx, key, t.Tag.Get("redis"), v.String())
+		case reflect.Struct:
+			if err := reflectStructHash(ctx, key, rdb, v, v.Type()); err != nil {
+				return err
+			}
+		default:
+			return errors.New("no support type")
+		}
+
+	}
+	return nil
 }
